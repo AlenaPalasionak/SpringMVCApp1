a1723e27a6d9381d4f29847bfb1263df6acf8364
Lena Palasionak <19lenka87@tut.by>
2022-03-26 09:54:33 +0300

    Spring MVC app:Get with parameters

--- src/main/java/org/palasionak/spring/controllers/FirstController.java
+++ src/main/java/org/palasionak/spring/controllers/FirstController.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller// создается объект, с методами
-//@RequestMapping("/first")// у адреса будет префикс first/
+@RequestMapping("/first")// у адреса будет префикс first/
 public class FirstController {
 
     @GetMapping("/hello")//хотим принимать на вход get-запросы
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surName = request.getParameter("surname");
+
+        System.out.println("Hello " + name +" "+ surName);
         return "first/hello";// вьюшки(представления) будут лежать в папке first во views
 
     }
