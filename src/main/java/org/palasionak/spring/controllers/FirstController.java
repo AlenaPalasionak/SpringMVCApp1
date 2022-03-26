69dd687e738f05cfdb67d0b66c17040c0687a831
Lena Palasionak <19lenka87@tut.by>
2022-03-26 10:42:47 +0300

    Spring MVC app:@requestParam

--- src/main/java/org/palasionak/spring/controllers/FirstController.java
+++ src/main/java/org/palasionak/spring/controllers/FirstController.java
@@ -3,13 +3,19 @@ package org.palasionak.spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller// создается объект, с методами
-//@RequestMapping("/first")// у адреса будет префикс first/
+@RequestMapping("/first")// у адреса будет префикс first/
 public class FirstController {
 
     @GetMapping("/hello")//хотим принимать на вход get-запросы
-    public String helloPage(@ReqestParam("name") String name, @RequestParam("SurName") String surname) {
+    public String helloPage(@RequestParam("name") String name,
+                            @RequestParam("SurName") String surName) {
+
+        System.out.println("Hello " + name +" "+ surName);
         return "first/hello";// вьюшки(представления) будут лежать в папке first во views
 
     }
@@ -19,4 +25,5 @@ public class FirstController {
 
     }
 }
-//public String helloPage(@ReqestParam("name") String name, @RequestParam("SurName") String surname) {
+
+//
