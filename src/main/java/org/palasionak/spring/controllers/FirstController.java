2f0695236cb9b0ab764c60c7eccdd0b3cc4bf9d4
Lena Palasionak <19lenka87@tut.by>
2022-03-28 20:02:24 +0300

    Spring MVC app: Calculator

--- src/main/java/org/palasionak/spring/controllers/FirstController.java
+++ src/main/java/org/palasionak/spring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.palasionak.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,20 +14,52 @@ public class FirstController {
 
     @GetMapping("/hello")//хотим принимать на вход get-запросы
     public String helloPage(@RequestParam(value = "name", required = false) String name, //required = false - для запроса без параметров
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname, Model model) {
 
-        System.out.println("Hello " + name +" "+ surname);
+        //  System.out.println("Hello " + name +" "+ surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
         return "first/hello";// вьюшки(представления) будут лежать в папке first во views
 
+/*
+по адресу /first/hello мы перейдем в метод помеченный аннотацией @GetMapping("/hello"). Метод возвращает путь к
+тому файлу html, в котором прописано, что будет отображаться: папка - first, файл - hello
+*/
     }
-    @GetMapping("/goodbye")
+
+    @GetMapping("/goodbye")// наберем этот адрес - перейдем в этот метод, этот метод вернёт путь к вьюшке
     public String goodbyeString() {
         return "first/goodbye";// вьюшки(представления) будут лежать в папке first во views
+    }
 
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a, @RequestParam("b") int b,
+                             @RequestParam("action") String action, Model model) {
+        double result;
+
+        switch (action) {
+            case "multiplication":
+                result = a*b;
+                break;
+            case"division":
+                result = a/(double)b;
+                break;
+            case "subtraction":
+                result = a-b;
+                break;
+            case "addition":
+                result = a+b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        model.addAttribute("result", result);// по ключу result поместим значение в переменную result. В html файле
+        // обратимся по этому ключу к значеию <p th:text="${result}"/>
+        return "first/calculator";
     }
 }
-//С аннотацией @RequestMapping – мы не можем сделать запрос без параметров. Можем используя - required = false -
-//А с HttpServletRequest – можно делать запрос без параметров – просто придет результат с null.
+
 
 
 
